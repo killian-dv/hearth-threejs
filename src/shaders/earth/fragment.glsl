716c2fe007eeb1f43b0df495a39ea6b5c4817db1
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-04-14 22:43:05 +0200

    ✨ : add Earth textures and update shader uniforms for day/night rendering

--- src/shaders/earth/fragment.glsl
+++ src/shaders/earth/fragment.glsl
@@ -2,11 +2,20 @@ varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
+uniform sampler2D uDayTexture;
+uniform sampler2D uNightTexture;
+uniform sampler2D uSpecularCloudsTexture;
+
 void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
-    vec3 color = vec3(vUv, 1.0);
+    vec3 color = vec3(0.0);
+
+    // day / night color
+    vec3 dayColor = texture(uDayTexture, vUv).rgb;
+    vec3 nightColor = texture(uNightTexture, vUv).rgb;
+    color = dayColor;
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
